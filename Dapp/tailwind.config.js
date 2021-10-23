6e264941f8e8bff2ac27d9ccb155e1f814d27143
Ehtisham <ehtishamhassan9@gmail.com>
2021-10-23 18:41:06 +0500

    Partially implemented new design

--- Dapp/tailwind.config.js
+++ Dapp/tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#e24717",
+                },
+                secondary: {
+                    DEFAULT: "#f0af6d",
+                    light: "#ffe2da"
+                },
+            },
+            borderRadius: {
+                '8xl': '3rem'
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
