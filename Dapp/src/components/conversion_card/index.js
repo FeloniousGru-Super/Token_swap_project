d301525dd3a8b2c9372b6fef76720446ca0084e7
Joao <up201706978@fe.up.pt>
2021-11-10 22:21:36 +0000

    uniswap front-end working ropsten

--- Dapp/src/components/conversion_card/index.js
+++ Dapp/src/components/conversion_card/index.js
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Switch from "../switch";
-import {
-    MdInfoOutline,
-    MdOutlineSettings,
-    MdArrowDownward,
-} from "react-icons/md";
+import { MdInfoOutline, MdOutlineSettings, MdArrowDownward } from "react-icons/md";
 import FromInputField from "./FromInputField";
 import ToInputField from "./ToInputField";
 import SelectToken from "../select_token_card";
 import Pool from "./pool";
+import mainnetSwapAbi from "./mainnetSwapAbi.json";
+import { Contract, utils } from "ethers";
+import { useContractFunction } from "@usedapp/core";
+
+export function initUniswapContract() {
+    const mainnetSwapInterface = new utils.Interface(mainnetSwapAbi);
+    return new Contract("0xe2C0Ae988413afD7a1EDe13E6e386b9cd9bE9E12", mainnetSwapInterface);
+}
+
+export function initApproveContract(tokenAddress) {
+    let abi = ["function approve(address _spender, uint256 _value) public returns (bool success)"];
+    const approveInterface = new utils.Interface(abi);
+    return new Contract(tokenAddress, approveInterface);
+}
+
+export function useContractMethod(contract, methodName) {
+    const { state, send } = useContractFunction(contract, methodName, {});
+    return { state, send };
+}
 
 const ConversionCard = ({
     changeScreen,
@@ -17,13 +32,32 @@ const ConversionCard = ({
     fromInput,
     toInput,
     accountAddress,
-    selectedSpeedOption, 
-    selectedToleranceOption, 
+    selectedSpeedOption,
+    selectedToleranceOption,
     customToleranceValue,
     customTransactionDetail,
-    gasPrice
+    gasPrice,
 }) => {
     const [tooltipVisibility, setTooltipVisibility] = useState(false);
+
+    const uniswapContract = initUniswapContract();
+    const { state: convertLunaChowToExactEthState, send: convertLunaChowToExactEth } =
+        useContractMethod(uniswapContract, "convertLunaChowToExactEth");
+    const { state: convertEthToExactLunaChowState, send: convertEthToExactLunaChow } =
+        useContractMethod(uniswapContract, "convertEthToExactLunaChow");
+
+    const approveWETHContract = initApproveContract("0xc778417E063141139Fce010982780140Aa0cD5Ab");
+    const approveLunaChowContract = initApproveContract(
+        "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"
+    );
+    const { state: approveWETHState, send: approveWETH } = useContractMethod(
+        approveWETHContract,
+        "approve"
+    );
+    const { state: approveLunaChowState, send: approveLunaChow } = useContractMethod(
+        approveLunaChowContract,
+        "approve"
+    );
 
     const handleInvertClick = () => {
         const temp = fromInput;
@@ -46,12 +80,12 @@ const ConversionCard = ({
 
     var fromTokenValue;
     var toTokenValue;
-    
-    function onToChange(value){
+
+    function onToChange(value) {
         toTokenValue = value;
     }
 
-    function onFromChange(value){
+    function onFromChange(value) {
         fromTokenValue = value;
     }
 
@@ -59,38 +93,70 @@ const ConversionCard = ({
         switch (selectedSpeedOption) {
             case 0:
                 return gasPrice;
-            case 1:            
+            case 1:
                 return gasPrice + 5;
             case 2:
-                return gasPrice + 10;                
+                return gasPrice + 10;
             default:
                 break;
         }
     }
 
     function getSlippage(selectedToleranceOption, customToleranceValue) {
-        if(customToleranceValue){
+        if (customToleranceValue) {
             return customToleranceValue;
         }
 
         switch (selectedToleranceOption) {
             case 0:
                 return 0.001;
-            case 1:            
+            case 1:
                 return 0.005;
             case 2:
-                return 0.01;                
+                return 0.01;
             default:
                 break;
         }
     }
 
-    function handleSwap(fromInput, toInput, toTokenValue, fromTokenValue) {
-        if(fromInput && toInput){
-            console.log(accountAddress,' swap ',  fromTokenValue,' from ', " ", fromInput.title, " to ", toTokenValue, " ", toInput.title," ",);
-            console.log("gas", getGas(selectedSpeedOption));
-            console.log("slippage", getSlippage(selectedToleranceOption, customToleranceValue));
-            console.log("deadline (min)", customTransactionDetail);
+    async function handleSwap() {
+        if (fromInput && toInput && fromTokenValue && toTokenValue) {
+            const _tokenIn = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+            const _tokenOut = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
+            const _amountIn = utils.parseEther(fromTokenValue.toString());
+            const _amountOut = utils.parseEther(toTokenValue.toString());
+            const _deadline = customTransactionDetail;
+            // console.log("gas", getGas(selectedSpeedOption));
+            // console.log("slippage", getSlippage(selectedToleranceOption, customToleranceValue));
+            // console.log("deadline (min)", customTransactionDetail);
+            console.log(fromInput["id"]);
+            if (fromInput["id"] === 1) {
+                await convertEthToExactLunaChow(_amountOut, _deadline, {
+                    value: _amountIn,
+                });
+            } else {
+                await convertLunaChowToExactEth(
+                    _tokenIn,
+                    _tokenOut,
+                    _amountIn,
+                    _amountOut,
+                    _deadline
+                ).then(() => {
+                    if (convertLunaChowToExactEthState.errorMessage === "execution reverted: STF") {
+                        if (_tokenIn === "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984") {
+                            approveLunaChow(
+                                "0xe2C0Ae988413afD7a1EDe13E6e386b9cd9bE9E12",
+                                "10000000000000000000"
+                            );
+                        } else if (_tokenIn === "0xc778417E063141139Fce010982780140Aa0cD5Ab") {
+                            approveWETH(
+                                "0xe2C0Ae988413afD7a1EDe13E6e386b9cd9bE9E12",
+                                "10000000000000000000"
+                            );
+                        }
+                    }
+                });
+            }
         }
     }
 
@@ -120,28 +186,19 @@ const ConversionCard = ({
                             <button>
                                 <MdInfoOutline
                                     size={24}
-                                    onClick={() =>
-                                        setTooltipVisibility((prev) => !prev)
-                                    }
-                                    onMouseEnter={() =>
-                                        setTooltipVisibility(true)
-                                    }
-                                    onMouseLeave={() =>
-                                        setTooltipVisibility(false)
-                                    }
+                                    onClick={() => setTooltipVisibility((prev) => !prev)}
+                                    onMouseEnter={() => setTooltipVisibility(true)}
+                                    onMouseLeave={() => setTooltipVisibility(false)}
                                 />
                                 <div
                                     className={`absolute w-64 bg-white p-2 rounded shadow right-0 lg:left-96 border ${
                                         tooltipVisibility ? "visible" : "hidden"
-                                    }`}
-                                >
+                                    }`}>
                                     <p>
-                                        Lorem ipsum dolor sit amet consectetur
-                                        adipisicing elit. Corporis, animi
-                                        officiis? Perferendis, dolor. Aliquam ad
-                                        beatae est amet ducimus facere omnis
-                                        dolorum commodi sint impedit accusamus
-                                        fuga, voluptatum, maxime sunt.
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Corporis, animi officiis? Perferendis, dolor. Aliquam ad
+                                        beatae est amet ducimus facere omnis dolorum commodi sint
+                                        impedit accusamus fuga, voluptatum, maxime sunt.
                                     </p>
                                 </div>
                             </button>
@@ -162,8 +219,7 @@ const ConversionCard = ({
                                 <button
                                     className="bg-secondary absolute -top-8 rounded-full p-2"
                                     type="button"
-                                    onClick={handleInvertClick}
-                                >
+                                    onClick={handleInvertClick}>
                                     <MdArrowDownward size={36} />
                                 </button>
                             </div>
@@ -178,25 +234,23 @@ const ConversionCard = ({
                             />
 
                             <div className="text-center pt-6">
-                                { accountAddress == true ? ( 
+                                {accountAddress == true ? (
                                     <button
                                         type="button"
                                         value="Connect to a Wallet"
                                         className="py-6 px-12 border-white border-2 bg-primary text-white rounded-full mt-2 shadow-md font-medium"
-                                        onClick = {() => changeScreen("connect_wallet")}
-                                    >
+                                        onClick={() => changeScreen("connect_wallet")}>
                                         <p>Connect to a Wallet</p>
                                     </button>
-                                ) : 
+                                ) : (
                                     <button
                                         type="button"
                                         value="Swap"
                                         className="py-6 px-12 border-white border-2 bg-primary text-white rounded-full mt-2 shadow-md font-medium"
-                                        onClick = {() => handleSwap(fromInput, toInput, toTokenValue, fromTokenValue)}
-                                    >
+                                        onClick={handleSwap}>
                                         <p>Swap</p>
                                     </button>
-                                }
+                                )}
                             </div>
                         </form>
                     )}
